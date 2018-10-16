e6490912534159660c4cba3cec479c327e33a622
tq <tq@tqdeMac.local>
2018-10-16 07:33:54 -0700

    add Notification class

---
+++ MyIns/Notification.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  MyIns
+//
+//  Created by tq on 2018/10/16.
+//  Copyright © 2018年 Jingyuan Bi. All rights reserved.
+//
+
+import Foundation
+class Notification{
+    var from: String? = ""
+    var objectId: String? = ""
+    var type: String? = ""
+    var id: String? =  ""
+    var timestamp: Int?
+    
+    
+    static func transformNotification(dict:[String: Any], notificationId: String) -> Notification{
+        let notification = Notification()
+        notification.id = notificationId
+        notification.objectId = dict["objectId"] as? String
+        notification.objectId = dict["type"] as? String
+        notification.timestamp = dict["timestamp"] as? Int
+    
+        return notification
+    }
+}
